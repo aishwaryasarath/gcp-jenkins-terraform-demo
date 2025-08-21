957d66333c7884cd40ae563af82141bfdda3ce1b
Aiswarya Sarath <49971693+aishwaryasarath@users.noreply.github.com>
2025-08-20 19:32:41 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,8 +4,10 @@ terraform {
     random = { source = "hashicorp/random", version = "~> 3.6" }
   }
 }
+
+# Let the provider read GOOGLE_APPLICATION_CREDENTIALS automatically.
 provider "google" {
-  project     = var.project_id
-  region      = var.region
-  credentials = file(var.credentials_json)
+  project = var.project_id
+  region  = var.region
+  # credentials = file(var.credentials_json)  <-- REMOVE THIS LINE
 }
